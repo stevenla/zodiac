ba0720fa624fc04b96cdfdbe4df3ba3eefb99e4b
Steven La <mrstevenla@gmail.com>
2022-06-23 12:55:47 -0700

    Updates

---
+++ precache-manifest.c8dc61dc14db9765524f7d9b8895602c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aeb626b3e0be8d8a43f52b732ff419a",
+    "url": "/zodiac/index.html"
+  },
+  {
+    "revision": "5a0297b6982c623c56fc",
+    "url": "/zodiac/static/css/main.22ed693c.chunk.css"
+  },
+  {
+    "revision": "f8d1ac038fd3d52147c8",
+    "url": "/zodiac/static/js/2.22efa503.chunk.js"
+  },
+  {
+    "revision": "5a0297b6982c623c56fc",
+    "url": "/zodiac/static/js/main.712517e4.chunk.js"
+  },
+  {
+    "revision": "d9c4e43cdf694a614536",
+    "url": "/zodiac/static/js/runtime~main.b240c23a.js"
+  }
+]);
